746d210e2378ffc46ab3adde7d98a4e93a77ceb8
Kevin Lugo Rosado <kevin.lugo@10.31.1.55>
2019-02-14 20:14:50 -0400

    Finished all the Specs for the first half of the Project and did some refactoring.

--- src/Game/Entities/Dynamic/Player.java
+++ src/Game/Entities/Dynamic/Player.java
@@ -14,238 +14,235 @@ import java.awt.image.BufferedImage;
  * The Frog.
  */
 public class Player extends EntityBase {
-    private Handler handler;
+	private Handler handler;
 
-    private Rectangle player;
-    private String facing = "UP";
-    private Boolean moving = false;
-    private int moveCoolDown=0;
-    private int index =0;
+	private Rectangle player;
+	private String facing = "UP";
+	private Boolean moving = false;
+	private int moveCoolDown = 0;
+	private int index = 0;
 
-    public Player(Handler handler) {
-        super(handler);
-        this.handler = handler;
-        this.handler.getEntityManager().getEntityList().add(this);
+	public Player(Handler handler) {
+		super(handler);
+		this.handler = handler;
+		this.handler.getEntityManager().getEntityList().add(this);
 
-        player = new Rectangle();
-        // see UpdatePlayerRectangle(Graphics g) for its usage.
-    }
+		player = new Rectangle();
+		// see UpdatePlayerRectangle(Graphics g) for its usage.
+	}
 
-    public void tick(){
+	public void tick() {
 
-        if(moving) {
-            animateMovement();
-        }
+		if (moving) {
+			animateMovement();
+		}
 
-        if(!moving){
-            move();
-        }
+		if (!moving) {
+			move();
+		}
 
-    }
+	}
 
-    private void reGrid() {
-        if(facing.equals("UP")) {
-            if(this.getX() % 64 >= 64 / 2 ) {
-                this.setX(this.getX() + (64 - this.getX() % 64));
-            }
-            else {
-                this.setX(this.getX() - this.getX() % 64);
-            }
-            setY(getY()-64);
-        }
-    }
+	private void reGrid() {
+		if (facing.equals("UP")) {
+			if (this.getX() % 64 >= 64 / 2) {
+				this.setX(this.getX() + (64 - this.getX() % 64));
+			} else {
+				this.setX(this.getX() - this.getX() % 64);
+			}
+			setY(getY() - 64);
+		}
+	}
+
 // this methods checks the player coordinates. It wont get the frog get out of bounds
-    private void PlayerBoundaries() {
-    //System.out.println("X = " + player.getX() + "  Y = "+ player.getY());
-    	if ( this.getX() <= (0))
-        this.setX(0);
-    	if ( this.getX() >= (640))
-            this.setX(576);
-    	
-    }
-    private void move(){
-    	PlayerBoundaries();
-    	
-    	if(moveCoolDown< 25){
-            moveCoolDown++;
-            
-        }
-        index=0;
+	private void PlayerBoundaries() {
+		// System.out.println("X = " + player.getX() + " Y = "+ player.getY());
+		if (this.getX() <= (0))
+			this.setX(0);
+		if (this.getX() >= (640))
+			this.setX(576);
 
-        /////////////////MOVE UP///////////////
-        if(handler.getKeyManager().keyJustPressed(KeyEvent.VK_W) && !moving && facing.equals("UP")){  	
-           
-        	moving=true;
-        	
-        }else if(handler.getKeyManager().keyJustPressed(KeyEvent.VK_W) && !moving && !facing.equals("UP")){
-            if(facing.equals("DOWN")) {
-                if(this.getX() % 64 >= 64 / 2 ) {
+	}
 
-                    this.setX(this.getX() + (64 - this.getX() % 64));
-                }
-                else {
-                    this.setX(this.getX() - this.getX() % 64);
-                }
-                setY(getY() + 64);
-            }
-            if(facing.equals("LEFT")) {
-                setY(getY() + 64);
-            }
-            if(facing.equals("RIGHT")) {
-                setX(getX()-64);
-                setY(getY()+64);
-            }
-            facing = "UP";
-        }
+	private void move() {
+		PlayerBoundaries();
 
-        /////////////////MOVE LEFT///////////////
-        else if(handler.getKeyManager().keyJustPressed(KeyEvent.VK_A) && !moving && facing.equals("LEFT")){
-            moving=true;
-        }else if(handler.getKeyManager().keyJustPressed(KeyEvent.VK_A) && !moving&& !facing.equals("LEFT")){
-            if(facing.equals("RIGHT")) {
-                setX(getX()-64);
-            }
-            reGrid();
-            facing = "LEFT";
-        }
+		if (moveCoolDown < 25) {
+			moveCoolDown++;
 
-        /////////////////MOVE DOWN///////////////
-        else if(handler.getKeyManager().keyJustPressed(KeyEvent.VK_S) && !moving && facing.equals("DOWN")){
-            moving=true;
-        }else if(handler.getKeyManager().keyJustPressed(KeyEvent.VK_S) && !moving && !facing.equals("DOWN")){
-            reGrid();
-            if(facing.equals("RIGHT")){
-                setX(getX()-64);
-            }
-            facing = "DOWN";
-        }
+		}
+		index = 0;
 
-        /////////////////MOVE RIGHT///////////////
-        else if(handler.getKeyManager().keyJustPressed(KeyEvent.VK_D) && !moving && facing.equals("RIGHT")){
-            moving=true;
-        }else if(handler.getKeyManager().keyJustPressed(KeyEvent.VK_D) && !moving&& !facing.equals("RIGHT")){
-            if(facing.equals("LEFT")) {
-                setX(getX()+64);
-            }
-            if(facing.equals("UP")) {
-                setX(getX()+64);
-                setY(getY()-64);
-            }
-            if(facing.equals("DOWN")) {
-                if(this.getX() % 64 >= 64 / 2 ) {
-                    this.setX(this.getX() + (64 - this.getX() % 64));
-                }
-                else {
-                    this.setX(this.getX() - this.getX() % 64);
-                }
-                setX(getX()+64);
-            }
-            facing = "RIGHT";
-        }
-    }
+		///////////////// MOVE UP///////////////
+		if (handler.getKeyManager().keyJustPressed(KeyEvent.VK_W) && !moving && facing.equals("UP")) {
 
-    private void animateMovement(){
-        if(index==8) {
-            moving = false;
-            index = 0;
-        }
-        moveCoolDown = 0;
-        index++;
-        switch (facing) {
-            case "UP":
-                if (this.getX() % 64 >= 64 / 2) {
-                    this.setX(this.getX() + (64 - this.getX() % 64));
-                } else {
-                    this.setX(this.getX() - this.getX() % 64);
-                }
-                setY(getY() - (8));
-                break;
+			moving = true;
 
-            case "LEFT":
-                setX(getX() - (8));
-                break;
+		} else if (handler.getKeyManager().keyJustPressed(KeyEvent.VK_W) && !moving && !facing.equals("UP")) {
+			if (facing.equals("DOWN")) {
+				if (this.getX() % 64 >= 64 / 2) {
 
-            case "DOWN":
-                if (this.getX() % 64 >= 64 / 2) {
-                    this.setX(this.getX() + (64 - this.getX() % 64));
-                } else {
-                    this.setX(this.getX() - this.getX() % 64);
-                }
-                setY(getY() + (8));
-                break;
+					this.setX(this.getX() + (64 - this.getX() % 64));
+				} else {
+					this.setX(this.getX() - this.getX() % 64);
+				}
+				setY(getY() + 64);
+			}
+			if (facing.equals("LEFT")) {
+				setY(getY() + 64);
+			}
+			if (facing.equals("RIGHT")) {
+				setX(getX() - 64);
+				setY(getY() + 64);
+			}
+			facing = "UP";
+		}
 
-            case "RIGHT":
-                setX(getX() + (8));
-                break;
+		///////////////// MOVE LEFT///////////////
+		else if (handler.getKeyManager().keyJustPressed(KeyEvent.VK_A) && !moving && facing.equals("LEFT")) {
+			moving = true;
+		} else if (handler.getKeyManager().keyJustPressed(KeyEvent.VK_A) && !moving && !facing.equals("LEFT")) {
+			if (facing.equals("RIGHT")) {
+				setX(getX() - 64);
+			}
+			reGrid();
+			facing = "LEFT";
+		}
 
-        }
-    }
+		///////////////// MOVE DOWN///////////////
+		else if (handler.getKeyManager().keyJustPressed(KeyEvent.VK_S) && !moving && facing.equals("DOWN")) {
+			moving = true;
+		} else if (handler.getKeyManager().keyJustPressed(KeyEvent.VK_S) && !moving && !facing.equals("DOWN")) {
+			reGrid();
+			if (facing.equals("RIGHT")) {
+				setX(getX() - 64);
+			}
+			facing = "DOWN";
+		}
 
-    public void render(Graphics g){
+		///////////////// MOVE RIGHT///////////////
+		else if (handler.getKeyManager().keyJustPressed(KeyEvent.VK_D) && !moving && facing.equals("RIGHT")) {
+			moving = true;
+		} else if (handler.getKeyManager().keyJustPressed(KeyEvent.VK_D) && !moving && !facing.equals("RIGHT")) {
+			if (facing.equals("LEFT")) {
+				setX(getX() + 64);
+			}
+			if (facing.equals("UP")) {
+				setX(getX() + 64);
+				setY(getY() - 64);
+			}
+			if (facing.equals("DOWN")) {
+				if (this.getX() % 64 >= 64 / 2) {
+					this.setX(this.getX() + (64 - this.getX() % 64));
+				} else {
+					this.setX(this.getX() - this.getX() % 64);
+				}
+				setX(getX() + 64);
+			}
+			facing = "RIGHT";
+		}
+	}
 
-        if(index>=8){
-            index=0;
-            moving = false;
-        }
+	private void animateMovement() {
+		if (index == 8) {
+			moving = false;
+			index = 0;
+		}
+		moveCoolDown = 0;
+		index++;
+		switch (facing) {
+		case "UP":
+			if (this.getX() % 64 >= 64 / 2) {
+				this.setX(this.getX() + (64 - this.getX() % 64));
+			} else {
+				this.setX(this.getX() - this.getX() % 64);
+			}
+			setY(getY() - (8));
+			break;
 
-        switch (facing) {
-            case "UP":
-                g.drawImage(Images.Player[index], getX(), getY(), getWidth(), -1 * getHeight(), null);
-                break;
-            case "DOWN":
-                g.drawImage(Images.Player[index], getX(), getY(), getWidth(), getHeight(), null);
-                break;
-            case "LEFT":
-                g.drawImage(rotateClockwise90(Images.Player[index]), getX(), getY(), getWidth(), getHeight(), null);
-                break;
-            case "RIGHT":
-                g.drawImage(rotateClockwise90(Images.Player[index]), getX(), getY(), -1 * getWidth(), getHeight(), null);
-                break;
-        }
+		case "LEFT":
+			setX(getX() - (8));
+			break;
 
+		case "DOWN":
+			if (this.getX() % 64 >= 64 / 2) {
+				this.setX(this.getX() + (64 - this.getX() % 64));
+			} else {
+				this.setX(this.getX() - this.getX() % 64);
+			}
+			setY(getY() + (8));
+			break;
 
-        UpdatePlayerRectangle(g);
+		case "RIGHT":
+			setX(getX() + (8));
+			break;
 
-    }
+		}
+	}
 
-    // Rectangles are what is used as "collisions." 
-    // The hazards have Rectangles of their own.
-    // This is the Rectangle of the Player.
-    // Both come in play inside the WorldManager.
-    private void UpdatePlayerRectangle(Graphics g) {
+	public void render(Graphics g) {
 
-        player = new Rectangle(this.getX(), this.getY(), getWidth(), getHeight());
+		if (index >= 8) {
+			index = 0;
+			moving = false;
+		}
 
-        if (facing.equals("UP")){
-            player = new Rectangle(this.getX(), this.getY() - 64, getWidth(), getHeight());
-        }
-        else if (facing.equals("RIGHT")) {
-            player = new Rectangle(this.getX() - 64, this.getY(), getWidth(), getHeight());
-        }
-    }
+		switch (facing) {
+		case "UP":
+			g.drawImage(Images.Player[index], getX(), getY(), getWidth(), -1 * getHeight(), null);
+			break;
+		case "DOWN":
+			g.drawImage(Images.Player[index], getX(), getY(), getWidth(), getHeight(), null);
+			break;
+		case "LEFT":
+			g.drawImage(rotateClockwise90(Images.Player[index]), getX(), getY(), getWidth(), getHeight(), null);
+			break;
+		case "RIGHT":
+			g.drawImage(rotateClockwise90(Images.Player[index]), getX(), getY(), -1 * getWidth(), getHeight(), null);
+			break;
+		}
 
-    @SuppressWarnings("SuspiciousNameCombination")
-    private static BufferedImage rotateClockwise90(BufferedImage src) {
-        int width = src.getWidth();
-        int height = src.getHeight();
+		UpdatePlayerRectangle(g);
 
-        BufferedImage dest = new BufferedImage(height, width, src.getType());
+	}
 
-        Graphics2D graphics2D = dest.createGraphics();
-        graphics2D.translate((height - width) / 2, (height - width) / 2);
-        graphics2D.rotate(Math.PI / 2, height / 2, width / 2);
-        graphics2D.drawRenderedImage(src, null);
+	// Rectangles are what is used as "collisions."
+	// The hazards have Rectangles of their own.
+	// This is the Rectangle of the Player.
+	// Both come in play inside the WorldManager.
+	private void UpdatePlayerRectangle(Graphics g) {
 
-        return dest;
-    }
+		player = new Rectangle(this.getX(), this.getY(), getWidth(), getHeight());
 
-    public Rectangle getPlayerCollision() {
-        return player;
-    }
-    
-    public String Getfacing() {
-    	return facing;
-    	
-    }
+		if (facing.equals("UP")) {
+			player = new Rectangle(this.getX(), this.getY() - 64, getWidth(), getHeight());
+		} else if (facing.equals("RIGHT")) {
+			player = new Rectangle(this.getX() - 64, this.getY(), getWidth(), getHeight());
+		}
+	}
+
+	@SuppressWarnings("SuspiciousNameCombination")
+	private static BufferedImage rotateClockwise90(BufferedImage src) {
+		int width = src.getWidth();
+		int height = src.getHeight();
+
+		BufferedImage dest = new BufferedImage(height, width, src.getType());
+
+		Graphics2D graphics2D = dest.createGraphics();
+		graphics2D.translate((height - width) / 2, (height - width) / 2);
+		graphics2D.rotate(Math.PI / 2, height / 2, width / 2);
+		graphics2D.drawRenderedImage(src, null);
+
+		return dest;
+	}
+
+	public Rectangle getPlayerCollision() {
+		return player;
+	}
+
+	public String Getfacing() {
+		return facing;
+
+	}
 
 }
